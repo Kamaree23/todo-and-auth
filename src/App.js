f1e248f29f8ff5a03b331e3fc409dfca81dd5369
Kamaree Robinson <kamareerobinson@gmail.com>
2022-12-11 22:39:17 -0500

    'finshed'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import Login from './login';
+import SignUp from './Signup';
+import Todo from './Todo';
+import Home from './Home'
+import Nav from './Nav'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  logMeIn = (user) => {
+    localStorage.setItem('user', JSON.stringify(user))};
+
+  addMessage = (msg, category) => {
+    this.setState({message: {message: msg, category: category}});
+  };
+  render() {
+    return (
+      <Router>
+        <div>
+        <Nav/>
+          
+          <Routes>
+
+            <Route path='/' element={<Home />}/>
+            <Route path='/login' element={<Login logMeIn={this.logMeIn} addMessage={this.addMessage}/>}/>
+            <Route path='/signup'element={<SignUp addMessage={this.addMessage}/>}/>
+            <Route path='/todo' element={<Todo />}/>
+        
+          </Routes>
+
+     
+
+        </div>
+      </Router>
+    )
+  }
 }
 
-export default App;
